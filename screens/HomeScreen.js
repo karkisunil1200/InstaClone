91410d0b382ac715e90b89d06d9772abf491d8c9
sunilkarki <karkisunil1200@gmail.com>
2022-01-17 14:41:14 -0600

    Added safeAreaView

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
