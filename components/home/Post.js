6c06ce8c270b8768a24ca8d82f8193147676b378
sunilkarki <karkisunil1200@gmail.com>
2022-01-18 14:13:46 -0600

    profile avatar showing on the side of the post

--- components/home/Post.js
+++ components/home/Post.js
@@ -1,23 +1,40 @@
 import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 
-const Post = () => {
+const Post = ({ post }) => {
   return (
     <View style={{ marginBottom: 70, borderWidth: 3, borderColor: "red" }}>
-      <PostHeader />
+      <PostHeader post={post} />
     </View>
   );
 };
 
 const PostHeader = ({ post }) => {
   return (
-    <View>
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        margin: 5,
+        alignIItems: "center",
+      }}
+    >
       <View>
-        <Image />
-        <Text>This is text uder images</Text>
+        <Image source={{ uri: post.profile_picture }} style={styles.story} />
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  story: {
+    width: 35,
+    height: 35,
+    borderRadius: 50,
+    marginLeft: 6,
+    borderWidth: 3,
+    borderColor: "#ff8501",
+  },
+});
 
 export default Post;
--- data/posts.js
+++ data/posts.js
@@ -19,7 +19,7 @@ export const POSTS = [
     user: USERS[1].user,
     likes: 32,
     caption: "First post and many to come...",
-    profile_picture: USERS[0].image,
+    profile_picture: USERS[1].image,
     comments: [
       { user: "kanchi", comment: "Wow your new post. " },
       { user: "hazel", comment: "you finally did it" },
@@ -31,7 +31,7 @@ export const POSTS = [
     user: USERS[2].user,
     likes: 542,
     caption: "sooo nice and wintry",
-    profile_picture: USERS[0].image,
+    profile_picture: USERS[2].image,
     comments: [
       { user: "kanchi", comment: "Wow your new post. " },
       { user: "hazel", comment: "you finally did it" },
